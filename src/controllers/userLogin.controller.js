52fe341f3dfadafb80d73ef87fc7c7362583c58c
Christian Montoya <chrimonlo@gmail.com>
2022-03-16 12:41:24 -0500

    Change validation of password

--- src/controllers/userLogin.controller.js
+++ src/controllers/userLogin.controller.js
@@ -87,8 +87,8 @@ const controllerUser = {
       const user = await User.findOne({ email })
 
       const isMatch =
-        // user === null ? false : await bcrypt.compare(password, user.passwordHash)
-        user === null ? false : true
+        user === null ? false : await bcrypt.compare(password, user.passwordHash)
+        /* user === null ? false : true */
       if (!isMatch) {
         res.status(401).json({
           error: 'Invalid password or user'
